4e14739021a22b98c2fd12ea5211f75db703dcd2
wsrogers3 <wsrogers3@catamount.wcu.edu>
2017-11-15 15:49:40 -0500

    Added a demo for the Finite State Machine.

--- FSMDemo.py
+++ FSMDemo.py
@@ -1,11 +1,46 @@
+import json
 import booleantools
 from booleantools import BooleanFunction
 from booleantools import FiniteStateMachine
+import copy
 
 def main():
-    r34 = BooleanFunction([[0,2],[1,2],[0,1],[1]], 3)
-    fsm = FiniteStateMachine(r34)
-    for row in fsm.adjMatr:
-        print(row)
+    with open("in/2res_classes.json") as fileIn:
+        f_list = [BooleanFunction(f, 5) for f in json.load(fileIn)["classes"]]
+    
+    for f in f_list:
+        fsm = FiniteStateMachine(f)
+        for line in fsm.adjMatr:
+            print(*line, sep="\t")
+        words = search(fsm, 0, 5)
+        legitWords = set([])
+        for word in getSubVectors(5, words):
+            tupledWord = tuple(word)
+            legitWords.add(tupledWord)
+
+        print("Words = ", legitWords)
+        print("\n\n")
+
+def getSubVectors(length, listStuff):
+    for i in listStuff:
+        if len(i) != length:
+            yield from getSubVectors(length, i)
+        elif isinstance(i[0], int):
+            yield i
+
+def search(fsm, start, numMore, pathTaken=[]):
+    if numMore == 0: 
+        return pathTaken
+    
+    out = []
+    for i in range(0, len(fsm.adjMatr)):
+        adj = [(x,weight) for x,weight in enumerate(fsm.adjMatr[i]) if weight is not None]
+        for i in adj:
+            newRow = fsm.adjMatr[i[0]]
+            newList = copy.deepcopy(pathTaken) + [i[1]]
+            out.extend([search(fsm, newRow, numMore - 1, newList)])
+        
+    return out 
+
 
 main()
