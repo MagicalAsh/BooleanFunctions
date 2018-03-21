932a7a88c0a41337e63715002e2f73c772de95ce
wsrogers3 <wsrogers3@catamount.wcu.edu>
2018-03-21 16:25:27 -0400

    Changed generate classes to work more generally

--- demo/generate_classes.py
+++ demo/generate_classes.py
@@ -1,15 +1,31 @@
 from booleantools import BooleanFunction
+import sys
 import booleantools as bt
 import itertools
 import multiprocessing as mp
 import json
 
 # linear part + quadratic part
-def analyze_polys(polys, que):
-    for poly in polys:
-        if poly.is_k_resilient(k=2) and not poly.is_affine():
-            que.put(poly)
-    que.put(None)
+def analyze_polys(poly_gens, que, thread_no, n):
+    for polys in poly_gens:
+        for poly in polys:
+            func = BooleanFunction(poly, n)
+            if func.is_k_resilient(k=n-3) and not func.is_affine():
+                que.put(poly)
+    
+    # Once all of the generators have executed
+    que.put(thread_no)
+
+def reduce_classes_dgen(class_list, new_f, n):
+    """
+    Produces a *MINIMALLY* reduced function list. This is by no means fully reduced.
+    """
+    for f in class_list:
+        if new_f in f:
+            return None
+
+    class_list.append(get_class(new_f, n))
+    return None
 
 def reduce_classes(func_list):
     class_list = []
@@ -22,48 +38,66 @@ def reduce_classes(func_list):
             class_list.append(f.get_orbit())
     return class_list
 
+def get_class(f, n):
+    perms = bt.Sym(n)
+    return [apply_permutation(f, sigma) for sigma in perms]
+
+def apply_permutation(poly, perm):
+    def apply_perm_to_monomial(perm,monomial):
+        out = []
+        for var in monomial:
+            out.append(perm[var])
+        return out
+       
+    out = [apply_perm_to_monomial(perm, i) for i in poly]
+    return out
+
 def powerset(iterable):
     s = list(iterable)
     return list(itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1)))
 
+def func_generator(lin_part, non_lin_parts):
+    lin = [[mon] for mon in lin_part]
+    for nonlinear in non_lin_parts:
+        yield lin + list(nonlinear)
+            
+
 def main():
-    nonlin = powerset(itertools.combinations([0,1,2,3,4], 2))
+    n = int(sys.argv[1])
+
+    nonlin = powerset(itertools.combinations(list(range(n)), 2))
     nonlin.remove(())
-    lin = powerset([0,1,2,3,4])
-    f_list = []
-    for nonlinear in nonlin:
-        for linear in lin:
-            func = []
-            for i in linear:
-                func.append([i])
-            for i in nonlinear:
-                func.append(list(i))
-            f_list.append(BooleanFunction(func, 5))
+    lin = powerset(list(range(n)))
     
-    print(len(f_list))
-    chunked = [f_list[i:i+500] for i in range(0, len(f_list), 500)]         
+    generator_list = [func_generator(linear, nonlin) for linear in lin]    
+    size = len(generator_list)//16
+    chunked = [generator_list[i:i+size] for i in range(0, len(generator_list), size)]         
+
     que = mp.Queue()
-    for lst in chunked:
-        thred = mp.Process(target=analyze_polys, args=(lst, que))
+    threads = []
+    for generators in chunked:
+        thred = mp.Process(target=analyze_polys, args=(generators, que, len(threads), n))
         thred.start()
+        threads.append(thred)
     
-    noneCnt = 0
+    deadCnt = 0
     f_out = []
-    while noneCnt < len(chunked):
+    while deadCnt < len(chunked):
         f = que.get()
-        if f is not None:
-            f_out.append(f)
-        else:
-            noneCnt += 1
+        if isinstance(f, list):
+            reduce_classes_dgen(f_out, f, n)
+        else: # it's an int
+            deadCnt += 1
+            threads[f].join()
     
-    print("reducing classes: there are %d functions to reduce" % (len(f_out)))
-    classes = reduce_classes(f_out)
-    for f_class in classes:
-        print(f_class[0].__repr__())
-    
-    with open("out/classes.json", "w") as outfile:
-        outfile.write(json.dumps({i: classes[i][0].listform for i in range(len(classes))}, indent=4))
+    f_out = reduce_classes([BooleanFunction(f[0], n) for f in f_out])
+
+    with open("out/classes_%dv.json" % n, "w") as outfile: 
+        outfile.write(json.dumps({i: f_out[i][0].listform for i in range(len(f_out))}, indent=4))
 
 if __name__ == "__main__":
-    main()
+    if(len(sys.argv) != 2):
+        print("USAGE: python3 generate_classes.py <n>")
+    else:
+        main()
 
